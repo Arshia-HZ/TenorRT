54fd108fb9dcefb73a8d5046397ee92b18d83c1f
Arshia Hossein Zadeh <arshiahosseinzadeh@yahoo.com>
2024-09-22 11:23:13 +0330

    fix calibration preprocess function

--- export_mobilenetv3.py
+++ export_mobilenetv3.py
@@ -40,8 +40,10 @@ class EngineCalibrator(trt.IInt8EntropyCalibrator2):
         Prepares the internal DataLoader for the CIFAR-10 test dataset and wraps it in a DataLoaderBatcher.
         """
         transform = transforms.Compose([
-            transforms.Resize((224, 224)),  # Resize images to MobileNetV3 input shape
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
         test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
